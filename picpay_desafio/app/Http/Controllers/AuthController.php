42fe03adbad3601a0967ed77103e7051bda0d904
carlos-augusto-carneiro <devcarlosaugusto@outlook.com>
2025-12-16 17:42:03 -0300

    Feat: Adicionado as routes do user, adicionado o controler de autenticação - Falta implementar a lógica

---
+++ picpay_desafio/app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        return view("");
+    }
+
+    public function register(Request $request)
+    {
+        return view("");
+    }
+    public function logout(Request $request)
+    {
+        return view("");
+    }
+    public function refreshLogin(Request $request)
+    {
+        return view("");
+    }
+}
